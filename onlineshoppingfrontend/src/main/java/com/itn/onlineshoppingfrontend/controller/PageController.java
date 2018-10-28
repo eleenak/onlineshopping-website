5f73696798cbe7d8adb30ebe5c8f4630aa9032c3
Eleena <kayasthaeleena42@gmail.com>
2018-10-28 21:36:30 +0545

    frontend static setup

--- onlineshoppingfrontend/src/main/java/com/itn/onlineshoppingfrontend/controller/PageController.java
+++ onlineshoppingfrontend/src/main/java/com/itn/onlineshoppingfrontend/controller/PageController.java
@@ -10,17 +10,26 @@ public class PageController {
 	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
 		ModelAndView mv=new ModelAndView("page");
-		mv.addObject("greeting","Welcome to spring");
+		mv.addObject("title","Home");
+		mv.addObject("userClickHome",true);
 		return mv;
 	}
-	@RequestMapping(value ="/test/{greeting}" )
-	public ModelAndView test(@PathVariable("greeting")String greeting) {
-		if(greeting==null) {
-			greeting="baby";
-		}
+	
+	@RequestMapping(value = { "/about" })
+	public ModelAndView about() {
 		ModelAndView mv=new ModelAndView("page");
-		
-		mv.addObject("greeting",greeting);
+		mv.addObject("title","About Us");
+		mv.addObject("userClickAbout",true);
 		return mv;
 	}
+	
+	@RequestMapping(value = { "/contact" })
+	public ModelAndView contact() {
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("title","Contact Us");
+		mv.addObject("userClickContact",true);
+		return mv;
+	}
+	
+	
 }
---
+++ onlineshoppingfrontend/src/main/webapp/assets/js/myapp.js
@@ -0,0 +1,17 @@
+$(function(){
+	//solving the active menu problem
+	switch(menu){
+	case 'Home':
+		$('#home').addClass('active');
+		break;
+	case 'About Us':
+		$('#about').addClass('active');
+		break;
+	case 'Contact Us':
+		$('#contact').addClass('active');
+		break;
+		default:
+			$('#home').addClass('active');
+			break;
+	}
+});
