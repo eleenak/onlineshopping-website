a216bd138d6565a3ada3d6723f911ab35051b111
Eleena <kayasthaeleena42@gmail.com>
2018-10-28 23:27:27 +0545

    backend connection

---
+++ onlineshoppingbackend/src/main/java/com/itn/onlineshoppingbackend/dao/CategoryDAO.java
@@ -0,0 +1,9 @@
+package com.itn.onlineshoppingbackend.dao;
+
+import java.util.List;
+
+import com.itn.onlineshoppingbackend.dto.Category;
+
+public interface CategoryDAO {
+	List<Category> list();
+}
---
+++ onlineshoppingbackend/src/main/java/com/itn/onlineshoppingbackend/daoimpl/CategoryDAOImpl.java
@@ -0,0 +1,52 @@
+package com.itn.onlineshoppingbackend.daoimpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.itn.onlineshoppingbackend.dao.CategoryDAO;
+import com.itn.onlineshoppingbackend.dto.Category;
+
+@Repository("categoryDAO")
+public class CategoryDAOImpl implements CategoryDAO {
+
+	private static List<Category> categories=new ArrayList<>();
+	
+	static {
+		Category category=new Category();
+		//adding first category inside the list
+		
+		category.setId(1);
+		category.setName("Television");
+		category.setDescription("this is a description");
+		category.setImageUrl("CAT_1.png");
+		
+		categories.add(category);
+		
+		//second category
+		category=new Category();
+		category.setId(2);
+		category.setName("Mobile");
+		category.setDescription("this is mobile description");
+		category.setImageUrl("CAT_2.png");
+		
+		categories.add(category);
+		
+		//3rd category
+		category=new Category();
+		category.setId(3);
+		category.setName("Laptop");
+		category.setDescription("this is a laptop description");
+		category.setImageUrl("CAT_3.png");
+		
+		categories.add(category);
+	}
+	
+	@Override
+	public List<Category> list() {
+		// TODO Auto-generated method stub
+		return categories;
+	}
+
+}
